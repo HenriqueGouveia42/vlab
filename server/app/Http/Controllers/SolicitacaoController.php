4db8fd475b4a6af69edd0a89a29a4d82d548da33
Henrique Gouveia <henriquesgo42@gmail.com>
2026-09-21 19:47:24 -0300

    feat: comentario para documentacao

--- server/app/Http/Controllers/SolicitacaoController.php
+++ server/app/Http/Controllers/SolicitacaoController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Models\Solicitacao; // Importanos aqui o modelo
 use App\Http\Requests\StoreSolicitacaoRequest; // Form request que valida a entrada antes de passar para o controller
 
 class SolicitacaoController extends Controller
 {
+    /*
+        Função responsável por criar uma nova solicitação.
+        O 'Store...' no começo do nome da função é para deixar claro
+        que ela cria uma nova solicitacao no banco de dados
+    */
     public function store(StoreSolicitacaoRequest $request){
 
         $dadosValidados = $request->validated();
