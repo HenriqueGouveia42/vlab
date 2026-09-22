c26a5576a88b51abe81b8b813af514286bbf94c6
Henrique Gouveia <henriquesgo42@gmail.com>
2026-09-22 00:59:58 -0300

    feat: implementacao das funcoes de busca por solicitacoes paginadas com filtro e de atualizar o status de uma solicitacao

--- server/app/Http/Controllers/SolicitacaoController.php
+++ server/app/Http/Controllers/SolicitacaoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\IndexSolicitacaoRequest;
+use App\Http\Requests\PatchStatusRequest;
 use App\Models\Solicitacao; // Importanos aqui o modelo
 use App\Http\Requests\StoreSolicitacaoRequest; // Form request que valida a entrada antes de passar para o controller
 
@@ -19,6 +21,55 @@ class SolicitacaoController extends Controller
         $solicitacao = Solicitacao::create($dadosValidados);
 
         return response()->json($solicitacao, 201);
+
+    }
+
+    public function index(IndexSolicitacaoRequest $request){
+
+        $query = Solicitacao::query();
+
+        // filtro por status
+        if($request->filled('status')){
+            $query->where('status', $request->status);
+        }
+
+        // filtro por categoria
+        if($request->filled('categoria')){
+            $query->where('categoria', $request->categoria);
+        }
+
+        // filtro por prioridade
+        if($request->filled('prioridade')){
+            $query->where('prioridade', $request->prioridade);
+        }
+
+        $solicitacoes = $query->paginate(10);
+
+        return response()->json($solicitacoes);
+
+    }
+
+    public function show(string $id){
+
+        $solicitacao = Solicitacao::findOrFail($id);
+
+        return response()->json($solicitacao);
+
+    }
+
+    public function updateStatus(PatchStatusRequest $request, string $id){
+
+        $solicitacao = Solicitacao::findOrFail($id);
+
+        try{
+
+            $solicitacao->updateStatus($request->status);
+
+            return response()->json($solicitacao);
+
+        }catch(\Exception $e){
+            return response()->json(['erro' => $e->getMessage()], 422);
+        }
 
     }
 }
