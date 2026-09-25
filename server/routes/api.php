db9c7ef429610bfc14a7aa194d5cd2ca4285db0b
Henrique Gouveia <henriquesgo42@gmail.com>
2026-09-24 22:02:08 -0300

    fix: eliminacao de resto de template

--- server/routes/api.php
+++ server/routes/api.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\SolicitacaoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum'); -> resto de templat 
 
 Route::post('/v1/solicitacoes', [SolicitacaoController::class, 'store']);
 
