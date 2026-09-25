d99bdfb57603e8eee169106aaf1d057eafa6e4a8
Henrique Gouveia <henriquesgo42@gmail.com>
2026-09-24 23:20:04 -0300

    feat: rota para checar saude da api

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\SolicitacaoController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 //Route::get('/user', function (Request $request) {
 //    return $request->user();
@@ -15,3 +15,21 @@ Route::get('/v1/solicitacoes', [SolicitacaoController::class, 'index']);
 Route::get('/v1/solicitacoes/{id}', [SolicitacaoController::class, 'show']);
 
 Route::patch('/v1/solicitacoes/{id}/status', [SolicitacaoController::class, 'updateStatus']);
+
+Route::get('/v1/health', function () {
+    try {
+        DB::connection()->getPdo();
+        
+        return response()->json([
+            'status' => 'ok',
+            'database' => 'connected'
+        ], 200);
+        
+    } catch (\Exception $e) {
+        return response()->json([
+            'status' => 'error',
+            'database' => 'disconnected',
+            'message' => 'Não foi possível ligar à base de dados.'
+        ], 500);
+    }
+});
